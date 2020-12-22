ed9a4c64b2f3bef31152335004b63f3518152267
Bumi Kim <71010959+kimbumi@users.noreply.github.com>
2020-12-22 19:31:21 +0800

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//typing animation script
+var type = new Typed(".typing", {
+    strings: ["UX Designer.", "Youtuber.", "Photographer."],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+});
+  
+const hamburger_icon = document.querySelector('#nav-bar .nav-bar .nav-list .hamburger');
+const nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
+
+hamburger_icon.addEventListener('click', () => {
+	nav_list.classList.toggle('open');
+	hamburger_icon.classList.toggle('active');
+});
