949b4dc19d0a832d396237771b996201f40937cd
Bumi Kim <71010959+kimbumi@users.noreply.github.com>
2020-12-28 10:39:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //typing animation script
 var type = new Typed(".typing", {
-    strings: ["UX Designer.", "Youtuber.", "Photographer."],
+    strings: ["UX Designer.", "YouTuber.", "Photographer."],
     typeSpeed: 80,
     backSpeed: 50,
     loop: true
